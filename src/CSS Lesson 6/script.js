ecf252b86268ce6fd607b077a069801277ce45eb
Irina <scotchwhicky@gmail.com>
2018-09-20 20:58:09 +0300

    add files

---
+++ src/CSS Lesson 6/script.js
@@ -0,0 +1,16 @@
+$(function () {
+	function load(currency, selector) {
+		$.get(
+			'https://api.exchangeratesapi.io/latest',
+			{'base':currency},
+			function(curr){
+				$(selector).text(Math.round(curr.rates.RUB))
+			}
+		);
+	}
+	load('USD', '.usd');
+	load('EUR', '.eur');
+
+});
+
+
